4a08fc34ac9e8cca37929b4979d89940af01ff53
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-10-09 17:33:37 -0500

    added logic to add element to counter.

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Header from "nav/Header";
 import "./index.scss";
 
-const App = () => (
+const App = () => {
+  const [ count, setCount ] = useState(0);
+  return (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
     <Header />
     <div>Name: host</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+    <div>Count {count}</div>
+    <div>
+      <button
+        onClick={()=> setCount(count+1) }
+        className='bg-indigo-800 text-white font-bold py-2 px-4 rounded'
+      >Add to Cart</button>
+    </div>
   </div>
-);
+)};
 ReactDOM.render(<App />, document.getElementById("app"));
