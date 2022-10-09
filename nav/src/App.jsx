1621a8c2f77ec8e6320edf583d54f7d96ef20a44
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-10-09 17:13:35 -0500

    added new simple header

--- nav/src/App.jsx
+++ nav/src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import "./index.scss";
+import Header from './Header';
+import './index.scss';
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <Header />
     <div>Name: nav</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
---
+++ nav/src/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () =>(
+    <header className='bg-blue-700 text-white font-bold text-3x1 p-5 flex'>
+        <div>Header</div>
+    </header>
+)
