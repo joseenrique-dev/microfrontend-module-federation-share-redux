2b5ced373d1b03687eecb1f26cb4a7563417e264
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-10-09 22:18:44 -0500

    install pkges and created the stores file with redux toolkit logic

---
+++ store/src/store.jsx
@@ -0,0 +1,28 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+
+
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    count: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.count += 1
+    },
+    clear: (state) => {
+      state.count = 0
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { increment, clear } = counterSlice.actions
+
+export const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer
+  },
+})
